6c737f1be44e9f3f7e9de024c2f7fa6c59625ce1
yuzuriha <wellmagg@gmail.com>
2018-12-11 23:42:38 +0800

    改密碼

--- cfg/cfg.inc.php
+++ cfg/cfg.inc.php
@@ -5,5 +5,5 @@ define ('SYSTEM_DBHOST','127.0.0.1');
 define ('SYSTEM_DBNAME_FISHER','fisher');
 define ('SYSTEM_DBNAME_PRAETOR','praetor');
 define ('SYSTEM_DBUSER','root');
-define ('SYSTEM_DBPWD','root');
+define ('SYSTEM_DBPWD','pwd');
 ?>
